26534d8d57c25c813a02e2ac7d7f2a46d8685ec4
沙滩上的鱼 <3516464843@qq.com>
2024-09-01 00:06:42 +0800

    feat: build the page

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+    base: "/v-singer/",
     plugins: [
       vue(),
       AutoImport({
