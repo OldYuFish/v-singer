187b41b7496e3b995c1a58cc312993b791a895ad
沙滩上的鱼 <3516464843@qq.com>
2024-09-01 00:30:51 +0800

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# vsinger-songs
--- src/views/common/exception/404.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <OFException
-    title="404 Not Found"
-    subTitle="您去向的页面不存在，请点击按钮或5s后自动返回主页"
-  />
-</template>
-<script lang="ts" setup>
-import OFException from "@/components/Exception/index.vue";
-</script>
